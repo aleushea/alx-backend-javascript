9bc17d485ac7576f213f7dfe7a13db1620b6e4d6
aleushea <aleushea@gmail.com>
2024-04-26 21:08:03 +0300

    0x00. ES6 Basics

---
+++ 0x00-ES6_basic/101-iterateThroughObject.js
@@ -0,0 +1,8 @@
+export default function iterateThroughObject(reportWithIterator) {
+    let result = reportWithIterator.next();
+  
+    while (!result.done) {
+      console.log(result.value);
+      result = reportWithIterator.next();
+    }
+  }
