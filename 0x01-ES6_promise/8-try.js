b565686f7f99ca8db63f782a8c25be0a89a506c1
aleushea <aleushea@gmail.com>
2024-04-26 22:05:00 +0300

    0x01. ES6 Promises

---
+++ 0x01-ES6_promise/8-try.js
@@ -0,0 +1,9 @@
+
+export default function divideFunction(numerator, denominator) {
+    if (denominator === 0) {
+      throw new Error('Cannot divide by 0');
+    }
+  
+    return numerator / denominator;
+  }
+  
